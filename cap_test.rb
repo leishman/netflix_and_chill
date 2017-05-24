bb656bc1094b312301226d94251a57c57da98935
Alexander Leishman <leishman3@gmail.com>
2017-05-23 17:20:30 -0700

    get data collection working from browser

--- cap_test.rb
+++ cap_test.rb
@@ -49,14 +49,32 @@ class NetflixRunner
     # Select user on account
     all('.avatar-wrapper').first.click
 
+    sleep 2
     # open video jawbone
     all('.slider-item-0').first.click
     page.save_screenshot('screenshot_test.png')
 
     # click play button to redirect to video page
     find('.jawBone .play').click
+    sleep 5
 
-    sleep 10
+    find('body').send_keys([:control, :shift, :alt, 'd'])
+
+    # sleep 60
+
+    180.times do
+      sleep 0.5
+      res = page.evaluate_script("document.getElementsByClassName('player-info')[0].children[0].value")
+      pos = res.match(/^Position: (\d+\.\d+)$/)[1]
+      buf_size_bytes = res.match(/^Buffer size in Bytes \(a\/v\): \d+ \/ (\d+)$/)[1]
+      buf_size_secs = res.match(/^Buffer size in Seconds \(a\/v\): \d+\.\d+ \/ (\d+\.\d+)$/)[1]
+      throughput = res.match(/^Throughput: (\d+) kbps$/)[1]
+      puts "Position: #{pos}"
+      puts "Buffer Size (Bytes): #{buf_size_bytes}"
+      puts "Buffer Size (secs): #{buf_size_secs}"
+      puts "Throughput: #{throughput}"
+      puts "\n\n---------------\n\n"
+    end
   end
 end
 
